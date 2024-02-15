9e987756d628f853bb633fc7837838312a67962b
Coder030 <garg2012030@gmail.com>
2024-02-15 21:58:16 +0530

    some good important minor changes

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,13 @@ import prisma from "./db";
 
 const expenses: string[] = [];
 export const app = express()
-app.use(cors())
-app.use(express.json())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
@@ -32,7 +31,7 @@ app.post('/', async (req, res) => {
   const expense = await prisma.expense.create({
     data: {
       way: req.body.expense['way'],
-      amount: req.body.expense['amount']
+      amount: parseInt(req.body.expense['amount'], 10)
     }
   })
   res.json(expense)
